78dffb913adc13308f2ee9862e5aefc63b60c938
Peter Inglesby <peter.inglesby@gmail.com>
2018-09-08 17:37:56 +0100

    Implement standings_against_bot

--- botany/tests/test_tournament.py
+++ botany/tests/test_tournament.py
@@ -18,7 +18,7 @@ class TournamentTests(TestCase):
             bot.refresh_from_db()
         cls.bots = cls.active_bots + cls.inactive_bots
 
-    def test_calculate_standings_with_no_games(self):
+    def test_standings_with_no_games(self):
         keys = ["num_played", "num_wins", "num_draws", "num_losses", "score"]
 
         expected_rows = [
@@ -27,14 +27,13 @@ class TournamentTests(TestCase):
         ]
 
         rows = [
-            [getattr(bot, k) for k in ["id"] + keys]
-            for bot in tournament.calculate_standings()
+            [getattr(bot, k) for k in ["id"] + keys] for bot in tournament.standings()
         ]
 
         self.assertEqual(rows, expected_rows)
 
     @given(st.results(num_bots=6, num_games=40))
-    def test_calculate_standings(self, results):
+    def test_standings(self, results):
         expected_rows_by_bot = {
             bot: {
                 "num_played": 0,
@@ -98,8 +97,86 @@ class TournamentTests(TestCase):
         ]
 
         rows = [
+            [getattr(bot, k) for k in ["id"] + keys] for bot in tournament.standings()
+        ]
+
+        self.assertEqual(rows, expected_rows)
+
+    @given(st.results(num_bots=6, num_games=40))
+    def test_standings_against_bot(self, results):
+        this_bot = self.active_bots[0]
+
+        expected_rows_by_bot = {
+            bot: {
+                "num_played": 0,
+                "num_wins": 0,
+                "num_draws": 0,
+                "num_losses": 0,
+                "score": 0,
+            }
+            for bot in self.active_bots
+        }
+
+        for bot1_ix, bot2_ix, score in results:
+            bot1 = self.bots[bot1_ix]
+            bot2 = self.bots[bot2_ix]
+
+            if bot1.user == bot2.user:
+                continue
+
+            actions.report_result(bot1.id, bot2.id, score)
+
+            if bot1 in self.inactive_bots or bot2 in self.inactive_bots:
+                continue
+
+            if this_bot not in [bot1, bot2]:
+                continue
+
+            row1 = expected_rows_by_bot[bot1]
+            row2 = expected_rows_by_bot[bot2]
+
+            row1["num_played"] += 1
+            row2["num_played"] += 1
+
+            if score == 1:
+                row1["num_wins"] += 1
+                row1["score"] += 1
+                row2["num_losses"] += 1
+                row2["score"] -= 1
+            elif score == 0:
+                row1["num_draws"] += 1
+                row2["num_draws"] += 1
+            elif score == -1:
+                row1["num_losses"] += 1
+                row1["score"] -= 1
+                row2["num_wins"] += 1
+                row2["score"] += 1
+            else:
+                assert False
+
+        bots = []
+        keys = ["num_played", "num_wins", "num_draws", "num_losses", "score"]
+
+        for bot, row in expected_rows_by_bot.items():
+            if bot == this_bot:
+                continue
+
+            for k in keys:
+                setattr(bot, k, row[k])
+            bots.append(bot)
+
+        def sortkey(bot):
+            return [-bot.score, bot.num_played, -bot.num_wins, bot.name]
+
+        sorted_bots = sorted(bots, key=sortkey)
+
+        expected_rows = [
+            [getattr(bot, k) for k in ["id"] + keys] for bot in sorted_bots
+        ]
+
+        rows = [
             [getattr(bot, k) for k in ["id"] + keys]
-            for bot in tournament.calculate_standings()
+            for bot in tournament.standings_against_bot(this_bot)
         ]
 
         self.assertEqual(rows, expected_rows)
--- botany/tournament.py
+++ botany/tournament.py
@@ -56,7 +56,7 @@ def all_unplayed_games():
     return unplayed_games
 
 
-def calculate_standings():
+def standings():
     """Calculate tournament standings.
 
     Returns queryset of active bots annotated with:
@@ -150,3 +150,102 @@ ORDER BY score DESC, num_played, num_wins DESC, b.name
     """
 
     return Bot.objects.raw(sql)
+
+
+def standings_against_bot(bot):
+    """Calculate tournament standings.
+
+    Returns queryset of active bots annotated with:
+        num_played
+        num_wins
+        num_draws
+        num_losses
+        score
+
+    where score is (num_wins - num_losses).
+
+    Bots are ordered according to:
+        ORDER BY score DESC, num_played, num_wins DESC, b.name
+
+    This is likely to be called often -- whenever the homepage is loaded -- so
+    the results could be cached, or we could use materialized views rather than
+    CTEs.
+    """
+
+    sql = """
+WITH active_bots AS (
+    SELECT * FROM botany_bot
+    WHERE is_active
+),
+
+annotated_games AS (
+    SELECT
+        bot1_id AS bot1_id,
+        bot2_id AS bot2_id,
+        score = 1 AS is_bot1_win,
+        score = 0 AS is_bot1_draw,
+        score = -1 AS is_bot1_loss,
+        score = -1 AS is_bot2_win,
+        score = 0 AS is_bot2_draw,
+        score = 1 AS is_bot2_loss
+    FROM botany_game AS g
+    INNER JOIN active_bots AS b1 ON g.bot1_id = b1.id
+    INNER JOIN active_bots AS b2 ON g.bot2_id = b2.id
+    WHERE b1.id = %(bot_id)s OR b2.id = %(bot_id)s
+),
+
+bot1_results AS (
+    SELECT
+        bot1_id AS bot_id,
+        COUNT(bot1_id) AS num_played,
+        COUNT(is_bot1_win OR NULL) AS num_wins,
+        COUNT(is_bot1_draw OR NULL) AS num_draws,
+        COUNT(is_bot1_loss OR NULL) AS num_losses
+    FROM annotated_games AS g
+    GROUP BY bot1_id
+),
+
+bot2_results AS (
+    SELECT
+        bot2_id AS bot_id,
+        COUNT(bot2_id) AS num_played,
+        COUNT(is_bot2_win OR NULL) AS num_wins,
+        COUNT(is_bot2_draw OR NULL) AS num_draws,
+        COUNT(is_bot2_loss OR NULL) AS num_losses
+    FROM annotated_games AS g
+    GROUP BY bot2_id
+),
+
+combined_results AS (
+    SELECT * FROM bot1_results
+    UNION ALL
+    SELECT * FROM bot2_results
+),
+
+summary_results AS (
+    SELECT
+        bot_id,
+        SUM(num_played) AS num_played,
+        SUM(num_wins) AS num_wins,
+        SUM(num_draws) AS num_draws,
+        SUM(num_losses) AS num_losses,
+        SUM(num_wins) - SUM(num_losses) AS score
+    FROM combined_results
+    GROUP BY bot_id
+)
+
+SELECT
+    b.*,
+    COALESCE(num_played, 0) AS num_played,
+    COALESCE(num_wins, 0) AS num_wins,
+    COALESCE(num_draws, 0) AS num_draws,
+    COALESCE(num_losses, 0) AS num_losses,
+    COALESCE(score, 0) AS score
+FROM active_bots b
+LEFT JOIN summary_results r ON b.id = r.bot_id
+WHERE b.id != %(bot_id)s
+ORDER BY score DESC, num_played, num_wins DESC, b.name
+    """
+
+    params = {"bot_id": bot.id}
+    return Bot.objects.raw(sql, params=params)
