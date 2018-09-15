6b772db18cf3befc4199f50fad5374beb6d3f210
Peter Inglesby <peter.inglesby@gmail.com>
2018-09-15 06:45:38 +0100

    Get SECRET_KEY from environment, and set ALLOWED_HOSTS

--- server/botany/settings.py
+++ server/botany/settings.py
@@ -29,12 +29,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "wntncqjx!9!4p0)osnzvdu)@wf(mg-!tj9brm=6tuk32kg+6_3"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["botany18.pyconuk.org"]
 
 
 AUTH_LOGIN_URL = os.environ["AUTH_LOGIN_URL"]
