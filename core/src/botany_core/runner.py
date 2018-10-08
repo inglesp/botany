inglesp/botany

commit 6e7955c0c944a6f66d9c590991c37800a40ff3db

Allows to set list of moves for botany play client

core/src/botany_core/runner.py
@@ -1,7 +1,6 @@
 import inspect
 import itertools
 import json
-import re
 import traceback
 from copy import copy, deepcopy
 from enum import Enum
@@ -45,9 +44,7 @@ def rerun_game(game, move_list):
     return boards
 
 
-def run_game(game, fn1, fn2, opcode_limit=None, display_board=False):
-    re.purge()  # See https://github.com/inglesp/botany/issues/48.
-
+def run_game(game, fn1, fn2, opcode_limit=None, display_board=False, move_list=None):
     def build_result(result_type, score, traceback=None, invalid_move=None):
         return Result(
             result_type=result_type,
@@ -64,13 +61,21 @@ def run_game(game, fn1, fn2, opcode_limit=None, display_board=False):
     states = [None, None]
     winning_scores = [1, -1]
     losing_scores = [-1, 1]
+    player_ixs = [0, 1]
     board = game.new_board()
-    move_list = []
+
+    if move_list is not None:
+        # If there is a list of moves, rerun the game
+        boards = rerun_game(game, move_list)
+        board = boards[-1]
+        if len(move_list) % 2 == 1:
+            # If the number of moves is odd, player 2 is the next to play
+            player_ixs = [1, 0]
 
     if display_board:
         print(game.render_text(board))
 
-    for player_ix in itertools.cycle([0, 1]):
+    for player_ix in itertools.cycle(player_ixs):
         token = game.TOKENS[player_ix]
         fn = [fn1, fn2][player_ix]
 

client/src/botany_client/client.py
@@ -106,7 +106,13 @@ def submit(path):
 @click.argument("path1")
 @click.argument("path2")
 @click.option("--opcode-limit", type=int, default=None, help="set to 0 for no limit")
-def play(path1, path2, opcode_limit):
+@click.option(
+    "--move-list",
+    type=str,
+    default=None,
+    help="List of moves, but only work if neither bot expects state.",
+)
+def play(path1, path2, opcode_limit, move_list):
     game = utils.load_game_module()
 
     def get_next_move_human(board):
@@ -151,6 +157,10 @@ def play(path1, path2, opcode_limit):
     if opcode_limit == 0:
         opcode_limit = None
 
+    # Initiate the move list parameter
+    if move_list is not None:
+        move_list = [int(move) for move in move_list]
+
     if not tracer.opcode_limit_supported:
         print("Opcode limiting not supported in this version of Python")
         print()
@@ -169,7 +179,12 @@ def play(path1, path2, opcode_limit):
         fn2 = wrap_bot_fn(mod2.get_next_move)
 
     result = runner.run_game(
-        game, fn1, fn2, opcode_limit=opcode_limit, display_board=True
+        game,
+        fn1,
+        fn2,
+        opcode_limit=opcode_limit,
+        display_board=True,
+        move_list=move_list,
     )
 
     if result.score == 1:

commit f247407ca9ad5f94e12aae0568c17cf86febce30

Fix failures introduced in 6e7955c

core/src/botany_core/runner.py
@@ -1,6 +1,7 @@
 import inspect
 import itertools
 import json
+import re
 import traceback
 from copy import copy, deepcopy
 from enum import Enum
@@ -45,6 +46,8 @@ def rerun_game(game, move_list):
 
 
 def run_game(game, fn1, fn2, opcode_limit=None, display_board=False, move_list=None):
+    re.purge()  # See https://github.com/inglesp/botany/issues/48.
+
     def build_result(result_type, score, traceback=None, invalid_move=None):
         return Result(
             result_type=result_type,
@@ -62,9 +65,11 @@ def run_game(game, fn1, fn2, opcode_limit=None, display_board=False, move_list=N
     winning_scores = [1, -1]
     losing_scores = [-1, 1]
     player_ixs = [0, 1]
-    board = game.new_board()
 
-    if move_list is not None:
+    if move_list is None:
+        board = game.new_board()
+        move_list = []
+    else:
         # If there is a list of moves, rerun the game
         boards = rerun_game(game, move_list)
         board = boards[-1]